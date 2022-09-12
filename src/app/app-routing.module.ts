c91873ec40f9ebe16209f39228fc2dc9e3603956
Jacob Stec <55162781+PretzelStec@users.noreply.github.com>
2022-09-12 07:07:41 -0500

    adding new test components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "test", component: TestComponent },
+    { path: "home", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
